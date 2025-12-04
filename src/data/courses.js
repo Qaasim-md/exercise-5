e129c88b3378c502a1a114480ebeba9418397966
Qaasim-md <qaasimdavids1@gmail.com>
2025-12-04 15:37:05 +0200

    add exercise

---
+++ src/data/courses.js
@@ -0,0 +1,56 @@
+export default [
+  {
+    id: 1,
+    title: "Classic  Masterclass",
+    chef: "Chef Gorden ",
+    level: "Intermediate",
+    price: 1500,
+    available: false,
+    image: "https://i.ibb.co/T6fTWJw/gordan.jpg"
+  },
+  {
+    id: 2,
+    title: "Classic Italian Cuisine",
+    chef: "Chef Alessandro Giovanni",
+    level: "Beginner",
+    price: 2500,
+    available: false,
+    image: "https://i.ibb.co/fGPC0x5x/chef.jpg"
+  },
+  {
+    id: 3,
+    title: "Seafood Masterclass",
+    chef: "Carlo Robertson",
+    level: "Advanced",
+    price: 5000,
+    available: true,
+    image: "https://i.ibb.co/5XdxSkc6/chef-4.jpg"
+  },
+  {
+    id: 4,
+    title: "Masterclass in Pastry",
+    chef: "Chef Luke Garcia",
+    level: "Intermediate",
+    price: 1400,
+    available: true,
+    image: "https://i.ibb.co/TxcbVxGy/chef-2.jpg"
+  },
+  {
+    id: 5,
+    title: "Roast Masterclass",
+    chef: "Chef Gazelle",
+    level: "Beginner",
+    price: 1400,
+    available: true,
+    image: "https://i.ibb.co/2YfrgDtk/chef-3.jpg"
+  },
+  {
+    id: 6,
+    title: "Masterclass of Desserts",
+    chef: "Chef Francisco Maria",
+    level: "Intermediate",
+    price: 2200,
+    available: false,
+    image: "https://i.ibb.co/sdY0mxLr/chef-1.jpg"
+  }
+]
